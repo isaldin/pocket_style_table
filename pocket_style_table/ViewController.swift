e08d903d14fa33576df11fe0e5432cfe05f2789b
saldin.iliya <saldin.iliya@gmail.com>
2016-08-04 17:54:33 +0600

    Initial Commit

---
+++ pocket_style_table/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pocket_style_table
+//
+//  Created by saldin on 04/08/16.
+//  Copyright © 2016 saldin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
